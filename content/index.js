5de417dec395d96aece2555608ad8dfea4a89640
shawcs <shawcs.t@gmail.com>
2016-03-21 13:58:49 +0100

    index

--- content/index.js
+++ content/index.js
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-
-var leboncoin = require('./index.html');
-var swig  = require('swig');
-var express = require('express');
-var app = express();
-
-app.use(express.static('./'));
-
-app.get('/process_get', function(req, res){
-	try {
-		swig.renderFile('./template.html', resultats,function(err, output){
-			if(err){
-				throw err;
-			}
-			res.end(output);	
-		});
-	
-   }
-   catch(ex){
-   		console.log(ex.toString());
-   	   	res.end("error");
-   }
-})
-
-var server = app.listen(8080, function () {
-
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log("Example app listening at http://%s:%s", host, port)
-})
-=======
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
